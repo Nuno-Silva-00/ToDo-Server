334e1c6d2a6aeb8f6d3070e815b4275fcae4d810
José Silva <Nuno.Silva.00@proton.me>
2023-10-30 19:43:14 +0000

    Add Password Hashing

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,32 +1,40 @@
 import USER from "../mongo/models/user.js";
-//still need to add hashing, and tokens 
+import bcrypt from 'bcrypt';
+
+//falta tokens 
 const createUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
         const userExists = await USER.findOne({ email });
-       
+
         if (userExists)
             return res.status(200).json({ message: 'Email already Exists' });
 
-        const newUser = await USER.create({ name, email, password });
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-        res.status(200).json({ id: newUser.id, name: newUser.name, email: newUser.email, allToDos: newUser.allToDos });
+        const newUser = await USER.create({ name, email, password: hashedPassword });
+
+        res.status(200).json({ id: newUser.id, name: newUser.name, allToDos: newUser.allToDos });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
-
+//falta token
 const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
         const userExists = await USER.findOne({ email });
-        const passwordMatch = password === userExists.password;
+
+        if (!userExists)
+            return res.status(400).json({ message: 'Email not Found!' });
+
+        const passwordMatch = await bcrypt.compare(password, userExists.password);
 
         if (passwordMatch)
-            return res.status(200).json({ id: userExists.id, name: userExists.name, email: userExists.email, allToDos: userExists.allToDos });
+            return res.status(200).json({ id: userExists.id, name: userExists.name, allToDos: userExists.allToDos });
         else
-            return res.status(400).json({ message: 'User not found!' });
+            return res.status(400).json({ message: 'Wrong Password!' });
 
     } catch (error) {
         res.status(500).json({ message: error.message });
